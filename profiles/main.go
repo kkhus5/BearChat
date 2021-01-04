319ed92ca097748bdd203fb4f640d12b76483274
kkhus5 <kkhus5@berkeley.edu>
2021-01-03 23:55:59 -0800

    [bug] changing origin control again

--- profiles/main.go
+++ profiles/main.go
@@ -37,7 +37,7 @@ func CORS(next http.Handler) http.Handler {
 
 		// Set headers
 		w.Header().Set("Access-Control-Allow-Headers", "Content-Type")
-		w.Header().Set("Access-Control-Allow-Origin", "54.226.6.116")
+		w.Header().Set("Access-Control-Allow-Origin", "http://54.226.6.116:3000")
 		w.Header().Set("Access-Control-Allow-Methods", "GET, POST, DELETE, OPTIONS")
 		w.Header().Set("Access-Control-Allow-Credentials", "true")
 
